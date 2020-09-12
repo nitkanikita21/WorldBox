a1653e58b633b5dca358344aea2e448d6f4ac097
VVSnitka <waleryvvs1@gmail.com>
2020-09-12 20:21:57 +0300

    add UI elements and controller methods

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.map = new System.Windows.Forms.PictureBox();
+            this.btn_generateRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.BackColor = System.Drawing.Color.Black;
+            this.map.Location = new System.Drawing.Point(6, 6);
+            this.map.Name = "map";
+            this.map.Size = new System.Drawing.Size(438, 438);
+            this.map.TabIndex = 0;
+            this.map.TabStop = false;
+            // 
+            // btn_generateRun
+            // 
+            this.btn_generateRun.Location = new System.Drawing.Point(450, 6);
+            this.btn_generateRun.Name = "btn_generateRun";
+            this.btn_generateRun.Size = new System.Drawing.Size(344, 23);
+            this.btn_generateRun.TabIndex = 1;
+            this.btn_generateRun.Text = "Generate";
+            this.btn_generateRun.UseVisualStyleBackColor = true;
+            this.btn_generateRun.Click += new System.EventHandler(this.btn_generateRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_generateRun);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox map;
+        private System.Windows.Forms.Button btn_generateRun;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace WorldBox
 {
     public partial class Form1 : Form
     {
+        public PictureBox canvas;
+        public Graphics controller;
         public Form1()
         {
             InitializeComponent();
+            canvas = map;
+            controller = canvas.CreateGraphics();
+        }
+
+        public void Generate()
+        {
+            //старт генерации
+
+            //конец генерации
+            controller.Save(); // сохранение пикселей
+        }
+
+        private void btn_generateRun_Click(object sender, EventArgs e)
+        {
+            Generate();
         }
     }
 }
